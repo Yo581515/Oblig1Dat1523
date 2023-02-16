ba86c963d2bc2a556c1fa573638ea25f16a78237
Yos581515 <yfessaha@gmail.com>
2023-02-16 16:59:01 +0100

    oblig 1 start 10'th Glider makes things smooth in fragment

--- app/src/main/java/com/example/oblig1dat153/fragments/ExampleDialog.java
+++ app/src/main/java/com/example/oblig1dat153/fragments/ExampleDialog.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,14 +22,19 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.bumptech.glide.Glide;
 import com.example.oblig1dat153.R;
 import com.example.oblig1dat153.adapters.MyAdapter;
 import com.example.oblig1dat153.model.Animal;
 import com.example.oblig1dat153.model.AnimalList;
+import com.example.oblig1dat153.utils.QuizUtils;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class ExampleDialog extends AppCompatDialogFragment {
-    Uri imageUri;
-    ImageView image;
+
+    ImageView imageView;
     MyAdapter adapter;
 
     // constructor
@@ -37,15 +43,20 @@ public class ExampleDialog extends AppCompatDialogFragment {
     }
 
 
-
-
     private final ActivityResultLauncher<Intent> launcher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == Activity.RESULT_OK
                         && result.getData() != null) {
-                    imageUri = result.getData().getData();
-                    image.setImageURI(imageUri);
+                    Uri imageUri = result.getData().getData();
+                    try {
+                        Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(
+                                getActivity().getContentResolver(),
+                                imageUri);
+                        QuizUtils.insertToImageView(bitmapImage, imageView);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
     );
@@ -59,12 +70,12 @@ public class ExampleDialog extends AppCompatDialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(view);
 
-        image = view.findViewById(R.id.image);
+        imageView = view.findViewById(R.id.image);
         TextView name_txt = view.findViewById(R.id.name);
         TextView wrong_name_1_txt = view.findViewById(R.id.wrong_name_1);
         TextView wrong_name_2_txt = view.findViewById(R.id.wrong_name_2);
 
-        image.setOnClickListener(new View.OnClickListener() {
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(
@@ -81,14 +92,14 @@ public class ExampleDialog extends AppCompatDialogFragment {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
-                        if (TextUtils.isEmpty(name_txt.getText().toString())){
+                        if (TextUtils.isEmpty(name_txt.getText().toString())) {
                             Toast.makeText(getActivity().getApplication(), "Please Enter a Name", Toast.LENGTH_SHORT).show();
                             return;
-                        }else{
+                        } else {
                             getDialog().dismiss();
                         }
 
-                        Bitmap bitmapImage= ((BitmapDrawable) image.getDrawable()).getBitmap();
+                        Bitmap bitmapImage = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
 
 
                         Animal animal = new Animal(
